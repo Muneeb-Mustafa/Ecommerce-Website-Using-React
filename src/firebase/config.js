2193ec89a1f1814cd669a5136f9181da4a6baffe
Muneeb Mustafa <muneeb.mustafa080@gmail.com>
2024-11-01 01:15:19 +0500

    first commit

---
+++ src/firebase/config.js
@@ -0,0 +1,39 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithEmailAndPassword, signOut, createUserWithEmailAndPassword, sendPasswordResetEmail } from "firebase/auth";
+import { getFirestore, doc, setDoc } from "firebase/firestore";
+import { getStorage, ref, uploadBytes, uploadBytesResumable } from "firebase/storage";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: " -O- ",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase services
+const auth = getAuth(app);
+const db = getFirestore(app); // Firestore instance
+const storage = getStorage(app);
+
+// Export Firebase services and functions
+export {
+  auth,
+  db,
+  storage,
+  signInWithEmailAndPassword,
+  signOut,
+  createUserWithEmailAndPassword,
+  sendPasswordResetEmail,
+  doc,
+  setDoc,
+  ref,
+  uploadBytes,
+  uploadBytesResumable
+};
