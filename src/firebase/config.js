85b3c073bc200277058f99b4e0015fb4b16e1a8e
Muneeb Mustafa <136226245+Muneeb-Mustafa@users.noreply.github.com>
2024-11-01 18:24:08 +0500

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -5,14 +5,15 @@ import { getStorage, ref, uploadBytes, uploadBytesResumable } from "firebase/sto
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: " -O- ",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: ""
+  apiKey: "AIzaSyAxuf00UUgiOrH-O-sRF1yJX3dMOLvhuVE",
+  authDomain: "shop-nest-278.firebaseapp.com",
+  projectId: "shop-nest-278",
+  storageBucket: "shop-nest-278.appspot.com",
+  messagingSenderId: "914929799860",
+  appId: "1:914929799860:web:7d6ba6c8752a67de8cfde1",
+  measurementId: "G-EV4B5G2JLD"
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
